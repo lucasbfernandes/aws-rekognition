7228243eb987e7fbfc5e0c3ddb8878877e71bd5b
mayconjrpacheco <mayconjrpacheco@gmail.com>
2017-10-08 17:54:51 -0300

    initial componentization

--- src/modules/Main/Home/index.vue
+++ src/modules/Main/Home/index.vue
@@ -1,11 +1,25 @@
 <template>
 	<div class="Home">
-		Home
+		<el-row type="flex" class="row-bg" justify="center">
+			Logo
+		</el-row>
+		<el-row type="flex" class="row-bg" justify="center">
+			<el-button @click="onClickPicture">Enviar foto</el-button>
+		</el-row>
+		<el-row type="flex" class="row-bg" justify="center">
+			<el-button>Sobre</el-button>
+		</el-row>
 	</div>
 </template>
 <script>
+
 	export default {
-		name: 'home'
+		name: 'home',
+		methods: {
+			onClickPicture() {
+				this.$router.push({ path: 'home' })
+			}
+		}
 	}
 </script>
 <style lang="scss" scoped src="./style.scss"></style>
--- src/modules/Main/Picture/index.vue
+++ src/modules/Main/Picture/index.vue
@@ -1,11 +1,40 @@
 <template>
-	<div class="Picture">
-		Picture
-	</div>
+  <div class="Picture">
+    <el-row type="flex" class="row-bg" justify="center">
+      <el-upload
+        action="https://jsonplaceholder.typicode.com/posts/"
+        list-type="picture-card"
+        :on-preview="handlePictureCardPreview"
+        :on-remove="handleRemove">
+        <i class="el-icon-plus"></i>
+      </el-upload>
+      <el-dialog v-model="dialogVisible" size="tiny">
+        <img width="100%" :src="dialogImageUrl" alt="">
+      </el-dialog>
+    </el-row>
+    <el-row type="flex" class="row-bg" justify="center">
+      <el-button>Comparar</el-button>
+    </el-row>
+  </div>
 </template>
 <script>
-	export default {
-		name: 'picture'
-	}
+  export default {
+    name: 'picture',
+    data() {
+        return {
+          dialogImageUrl: '',
+          dialogVisible: false
+        };
+      },
+    methods: {
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePictureCardPreview(file) {
+        this.dialogImageUrl = file.url;
+        this.dialogVisible = true;
+      }
+    }
+  }
 </script>
 <style lang="scss" scoped src="./style.scss"></style>
