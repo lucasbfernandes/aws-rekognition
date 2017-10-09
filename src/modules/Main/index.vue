69d1aa3f1473ff6fd6f94cf710028c868d26635c
Lucas Borges Fernandes <lucasbfernandes94@gmail.com>
2017-10-09 02:33:47 -0300

    adding images - organizing imports

--- src/modules/Main/index.vue
+++ src/modules/Main/index.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="Main">
     <router-view></router-view>
+    <header class="Main__header">
+      <div class="Main__header__logo-container">
+        <img class="Main__header__logo" src="/src/core/images/logo-star-wars.svg" alt="Logo Aws"/>
+      </div>
+    </header>
     <footer class="Main__footer">
-      <div class="Main__partners-container">
-        <img class="Main__logo" src="/src/core/images/logo-aws-2.svg" alt="Logo Aws"/>
-        <img class="Main__logo" src="/src/core/images/logo-mandic.png" alt="Logo Mandic"/>
+      <div class="Main__footer__logo-container">
+        <img class="Main__footer__logo" src="/src/core/images/logo-aws-2.svg" alt="Logo Aws"/>
+        <img class="Main__footer__logo" src="/src/core/images/logo-mandic.png" alt="Logo Mandic"/>
       </div>
     </footer>
   </div>
