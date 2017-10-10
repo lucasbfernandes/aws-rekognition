81faa080c6824f7ee8a451ec333699f9b009dfc2
Lucas Borges Fernandes <lucasbfernandes94@gmail.com>
2017-10-09 23:52:43 -0300

    fix Main layouts :metal:

--- src/modules/Main/index.vue
+++ src/modules/Main/index.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="Main">
     <router-view></router-view>
-    <header class="Main__header">
-      <div class="Main__header__logo-container">
-        <img class="Main__header__logo" src="/src/core/images/logo-star-wars.svg" alt="Logo Aws"/>
-      </div>
+    <header class="Main__description Main__description--header">
+      <el-row type="flex" class="row-bg" justify="start">
+        <el-col :span="24">
+          <div class="Main__logo Main__logo--header">
+            <img class="Main__logo__img" src="/src/core/images/logo-star-wars.svg" alt="Logo Aws"/>
+          </div>
+        </el-col>
+      </el-row>
     </header>
-    <footer class="Main__footer">
-      <div class="Main__footer__logo-container">
-        <img class="Main__footer__logo" src="/src/core/images/logo-aws-2.svg" alt="Logo Aws"/>
-        <img class="Main__footer__logo" src="/src/core/images/logo-mandic.png" alt="Logo Mandic"/>
-      </div>
+    <footer class="Main__description Main__description--footer">
+      <el-row type="flex" class="row-bg" justify="end">
+        <el-col :span="24">
+          <div class="Main__logo Main__logo--footer">
+            <img class="Main__logo__img" src="/src/core/images/logo-aws-2.svg" alt="Logo Aws"/>
+            <img class="Main__logo__img" src="/src/core/images/logo-mandic.png" alt="Logo Mandic"/>
+          </div>
+        </el-col>
+      </el-row>
     </footer>
   </div>
 </template>
