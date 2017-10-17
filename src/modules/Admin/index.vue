f9dc9c18c3e2accda7e7063233a3188e412d9489
mayconjrpacheco <mayconjrpacheco@gmail.com>
2017-10-17 00:40:52 -0200

    Change page admin

--- src/modules/Admin/index.vue
+++ src/modules/Admin/index.vue
@@ -1,11 +1,91 @@
 <template>
-	<div class="Admin">
-		Admin
-	</div>
+  <div class="Admin">
+    <header class="Admin__header">
+      <el-row type="flex" class="row-bg" justify="center">
+        <el-col :span="24" justify="center">
+          <div class="Admin__logo Admin__logo--header">
+            <img class="Admin__logo__img" src="../../core/images/logo-star-wars.svg" alt="Logo Aws"/>
+          </div>
+        </el-col>
+      </el-row>
+    </header>
+
+    <div class="Admin__carrousel">
+      <el-carousel  :interval="4000" type="card" >
+        <el-carousel-item v-for="item in listUsers" :key="item.id">
+          <div class="Admin__carrousel__item">
+            <el-row type="flex" class="row-bg" justify="center">
+              <el-col :span="24" justify="center">
+                <h2>{{item.username}}</h2>
+                <img class="Admin__carrousel__item__picture" :src="item.picture">
+              </el-col>
+              <el-col :span="24" justify="center">
+                <h2>{{item.character_name}}</h2>
+                <img class="Admin__carrousel__item__character" :src="item.character_picture">
+              </el-col>
+            </el-row>
+          </div>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+  </div>
 </template>
 <script>
-	export default {
-		name: 'admin'
-	}
+  import { mapActions } from 'vuex';
+  import axios from 'axios';
+  import SYSTEM from '@core/constants/system';
+
+  export default {
+    name: 'admin',
+
+    data() {
+      return {
+        listUsers: null,
+      }
+    },
+
+    mounted() {
+      this.requestListUserTv();
+    },
+
+    methods: {
+      ...mapActions({
+        setLoading: 'setLoading',
+      }),
+
+      configForListUserTv() {
+        return {
+          method: 'get',
+          url: 'http://ec2-34-229-73-88.compute-1.amazonaws.com/ListResult',
+          headers: {'Content-Type': 'application/json' },
+          json: true
+        };
+      },
+
+      onRequestListUserTvSuccess(res) {
+        this.listUsers = res.data;
+      },
+
+      onRequestListUserTvError(error) {
+        console.log(error);
+      },
+
+      requestListUserTv() {
+        let config = this.configForListUserTv();
+        axios(config).then(
+          res => this.onRequestListUserTvSuccess(res),
+          error => this.onRequestListUserTvError(error)
+        );
+      },
+
+      reactorForRequest() {
+        setTimeout( res => {
+          this.requestListUserTv();
+          this.reactorForRequest();
+        }, SYSTEM.TIME_FOR_REQUEST );
+      },
+
+    }
+  }
 </script>
 <style lang="scss" scoped src="./style.scss"></style>
